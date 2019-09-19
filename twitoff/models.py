17c67a1341279fd45327c24a06a9ac791babb31c
Nick Burkhalter <nickburkhalter@Nicks-MacBook-Pro.local>
2019-09-18 20:32:04 -0700

    application updated

--- twitoff/models.py
+++ twitoff/models.py
@@ -3,21 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
-# Create user class
+
 class User(DB.Model):
-    '''Instantiate user class'''
-    id = DB.Column(DB.Integer, primary_key=True)
-    name = DB.Column(DB.String(20), unique=True, nullable=False)
+    """Twitter users that we pull and analyze tweets for."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
-    '''Instantiate tweet class'''
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    """Tweets."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # Allowing for full + links
     embedding = DB.Column(DB.PickleType, nullable=False)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
